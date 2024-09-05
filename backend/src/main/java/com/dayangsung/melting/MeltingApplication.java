a2a128cd62a2c61fc1785af244c4f33711b6b574
유승현 <fview@naver.com>
2024-09-05 14:23:05 +0900

    init dev-be
    Jira: S11P21A701-313

---
+++ backend/src/main/java/com/dayangsung/melting/MeltingApplication.java
@@ -0,0 +1,13 @@
+package com.dayangsung.melting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeltingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeltingApplication.class, args);
+	}
+
+}
