c0df53d5569d80aa461f592bf45cf43ca878c976
유승현 <fview@naver.com>
2024-09-11 08:12:53 +0900

    feat: member에 성별 필드 추가
    Jira: S11P21A701-449

---
+++ backend/src/main/java/com/dayangsung/melting/domain/member/enums/Gender.java
@@ -0,0 +1,16 @@
+package com.dayangsung.melting.domain.member.enums;
+
+public enum Gender {
+	MALE("male"),
+	FEMALE("female");
+
+	private final String gender;
+
+	Gender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+}
