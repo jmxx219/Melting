d9d663521f689daadd9623684b0acf17f0c545e6
유승현 <fview@naver.com>
2024-09-10 15:06:22 +0900

    feat: security config 설정
    Jira: S11P21A701-398

---
+++ backend/src/main/java/com/dayangsung/melting/global/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.dayangsung.melting.global.config;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.dayangsung.melting.domain.auth.service.AuthService;
+import com.dayangsung.melting.global.filter.JwtFilter;
+import com.dayangsung.melting.global.handler.LoginSuccessHandler;
+import com.dayangsung.melting.global.util.CookieUtil;
+import com.dayangsung.melting.global.util.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final AuthService authService;
+	private final LoginSuccessHandler loginSuccessHandler;
+	private final JwtUtil jwtUtil;
+	private final CookieUtil cookieUtil;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.addFilterBefore(new JwtFilter(jwtUtil, cookieUtil), UsernamePasswordAuthenticationFilter.class)
+			.oauth2Login(oauth2 -> oauth2
+				.userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+					.userService(authService))
+				.successHandler(loginSuccessHandler))
+			.authorizeHttpRequests(authorize ->
+				authorize
+					.requestMatchers(
+						"/api/v1/login/oauth2/code/*")
+					.permitAll()
+					.anyRequest()
+					.authenticated())
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return http.build();
+	}
+
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+		configuration.setAllowedMethods(Collections.singletonList("*"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(List.of("Authorization", "Set-Cookie", "Content-Type"));
+		configuration.setMaxAge(3600L);
+
+		configuration.setExposedHeaders(List.of("Authorization", "Set-Cookie"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
