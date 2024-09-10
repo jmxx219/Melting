eb95c4de5ac2217e17296d32b12e102bfe414684
유승현 <fview@naver.com>
2024-09-10 15:01:42 +0900

    feat: jwt 인증 인가 기능 추가
    Jira: S11P21A701-400

---
+++ backend/src/main/java/com/dayangsung/melting/global/filter/JwtFilter.java
@@ -0,0 +1,57 @@
+package com.dayangsung.melting.global.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.dayangsung.melting.global.util.CookieUtil;
+import com.dayangsung.melting.global.util.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+	private final JwtUtil jwtUtil;
+	private final CookieUtil cookieUtil;
+
+	public JwtFilter(JwtUtil jwtUtil, CookieUtil cookieUtil) {
+
+		this.jwtUtil = jwtUtil;
+		this.cookieUtil = cookieUtil;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		String accessToken = cookieUtil.getCookie(request, "access_token");
+		String refreshToken = cookieUtil.getCookie(request, "refresh_token");
+
+		if (accessToken == null || refreshToken == null) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		if (jwtUtil.isValidate(accessToken) && jwtUtil.isExpired(accessToken)) {
+			accessToken = jwtUtil.reissueToken(request, response, refreshToken);
+			if (accessToken == null) {
+				filterChain.doFilter(request, response);
+			}
+			cookieUtil.updateCookie(request, response, "access_token", accessToken);
+		}
+
+		Authentication authentication = new UsernamePasswordAuthenticationToken(
+			jwtUtil.getEmail(accessToken),
+			null,
+			null);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		filterChain.doFilter(request, response);
+	}
+}
---
+++ backend/src/main/java/com/dayangsung/melting/global/util/JwtUtil.java
@@ -0,0 +1,115 @@
+package com.dayangsung.melting.global.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.dayangsung.melting.domain.auth.dto.RedisToken;
+import com.dayangsung.melting.domain.auth.repository.RedisRepository;
+import com.dayangsung.melting.domain.auth.service.AuthService;
+
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+	private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 30;
+	private final SecretKey secretKey;
+	private final RedisRepository redisRepository;
+	private final AuthService authService;
+	private final CookieUtil cookieUtil;
+
+	public JwtUtil(@Value("${spring.jwt.secret}") String secretKey,
+		RedisRepository redisRepository,
+		AuthService authService, CookieUtil cookieUtil) {
+		this.secretKey = new SecretKeySpec(
+			secretKey.getBytes(StandardCharsets.UTF_8),
+			Jwts.SIG.HS256.key().build().getAlgorithm());
+		this.redisRepository = redisRepository;
+		this.authService = authService;
+		this.cookieUtil = cookieUtil;
+	}
+
+	public String getEmail(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("email", String.class);
+	}
+
+	public Boolean isExpired(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration()
+			.before(new Date());
+	}
+
+	public String createAccessToken(String email) {
+		return Jwts.builder()
+			.claim("email", email)
+			.issuedAt(new Date(System.currentTimeMillis()))
+			.expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRE_TIME))
+			.signWith(secretKey)
+			.compact();
+	}
+
+	public String reissueToken(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+		RedisToken redisToken = redisRepository.findRedisTokenByRefreshToken(refreshToken);
+
+		if (redisToken == null) {
+			cookieUtil.deleteJwtCookies(request);
+			authService.logout();
+			return null;
+		}
+
+		String accessToken = createAccessToken(redisToken.getEmail());
+		redisRepository.save(RedisToken.builder()
+			.email(redisToken.getEmail())
+			.refreshToken(createRefreshToken(redisToken.getEmail()))
+			.build());
+		cookieUtil.updateCookie(request, response, "access_token", accessToken);
+		cookieUtil.updateCookie(request, response, "refresh_token", refreshToken);
+
+		return accessToken;
+	}
+
+	public String createRefreshToken(String email) {
+		String refreshToken = UUID.randomUUID().toString();
+
+		redisRepository.save(RedisToken.builder()
+			.refreshToken(refreshToken)
+			.email(email)
+			.build());
+
+		System.out.println(refreshToken);
+		return refreshToken;
+	}
+
+	public boolean isValidate(String token) {
+		try {
+			Jwts.parser()
+				.verifyWith(secretKey)
+				.build()
+				.parseSignedClaims(token);
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
