11cc588c0e5657dce974b1fd0dc01ffba310a0ee
유승현 <fview@naver.com>
2024-09-10 15:01:42 +0900

    feat: jwt 인증 인가 기능 추가
    Jira: S11P21A701-400

---
+++ backend/src/main/java/com/dayangsung/melting/domain/auth/repository/RedisRepository.java
@@ -0,0 +1,9 @@
+package com.dayangsung.melting.domain.auth.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dayangsung.melting.domain.auth.dto.RedisToken;
+
+public interface RedisRepository extends CrudRepository<RedisToken, Long> {
+	RedisToken findRedisTokenByRefreshToken(String refreshToken);
+}
