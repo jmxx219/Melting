1c47651bf0ee443032b63559a0be9f347855f6ed
이보연 <btothey99@ewhain.net>
2024-09-11 16:20:07 +0900

    feat: 회원가입 페이지 UI 구현
    Jira: S11P21A701-381

--- frontend/melting/src/routers/index.tsx
+++ frontend/melting/src/routers/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LoginPage from '@/pages/Login/start'
+import LoginPage from '@/pages/Login/Login'
+import SignupPage from '@/pages/Login/Signup'
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   )
