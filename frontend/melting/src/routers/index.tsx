34ecd3c6cccdec09347104da2195da7cf83ff93c
이보연 <btothey99@ewhain.net>
2024-09-11 11:06:38 +0900

    rename: 시작화면 경로 변경
    Jira: S11P21A701-380

--- frontend/melting/src/routers/index.tsx
+++ frontend/melting/src/routers/index.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LoginPage from '@/pages/Login'
+import LoginPage from '@/pages/Login/start'
 
 export default function AppRoutes() {
   return (
