ad30e71bc6f65555e2879cb37f979fe1b83cdfc2
DESKTOP-MMCQPV1\SSAFY <breakking@naver.com>
2024-09-11 17:17:28 +0900

    feat: 타이틀 컴포넌트 추가

---
+++ frontend/melting/src/pages/music/MusciMain.tsx
@@ -0,0 +1,9 @@
+import LineTitle from '@/components/music/LineTitle'
+
+export default function MusciMain() {
+  return (
+    <div>
+      <LineTitle titles={['곡 제작 유형을', '선택해주세요']}></LineTitle>
+    </div>
+  )
+}
