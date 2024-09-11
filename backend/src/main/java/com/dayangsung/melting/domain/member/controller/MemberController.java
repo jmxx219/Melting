12c7d423a33d6599d45d50525124462f7ccdefbc
유승현 <fview@naver.com>
2024-09-11 16:53:06 +0900

    feat: 닉네임 중복 검사
    Jira: S11P21A701-449

--- backend/src/main/java/com/dayangsung/melting/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/dayangsung/melting/domain/member/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.dayangsung.melting.domain.member.controller;
 
+import static com.dayangsung.melting.global.common.response.enums.ErrorMessage.*;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dayangsung.melting.domain.auth.dto.CustomOAuth2User;
@@ -23,6 +27,15 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberController {
 
 	private final MemberService memberService;
+
+	@GetMapping("/nickname-check")
+	public ApiResponse<?> nicknameCheck(@RequestParam String nickname) {
+		if (!memberService.nicknameCheck(nickname)) {
+			return ApiResponse.ok(null);
+		} else {
+			return ApiResponse.error(DUPLICATE_NICKNAME.getErrorMessage());
+		}
+	}
 
 	@PatchMapping("/init")
 	public ApiResponse<MemberInitResponseDto> init(
