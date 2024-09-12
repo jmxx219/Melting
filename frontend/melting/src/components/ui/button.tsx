d8ac00627b2db3634be68de0871b7dde5f1d9c0e
이보연 <btothey99@ewhain.net>
2024-09-12 13:53:39 +0900

    style: 버튼, input css 수정
    Jira: S11P21A701-388

--- frontend/melting/src/components/ui/button.tsx
+++ frontend/melting/src/components/ui/button.tsx
@@ -5,16 +5,20 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none',
   {
     variants: {
       variant: {
         default: 'bg-primary-400 text-black hover:bg-primary-400/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-primary-400 bg-white hover:bg-primary-400 hover:text-white',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline:
+          'border border-primary-400 bg-white hover:bg-primary-400 hover:text-white',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-gray hover:text-gray',
         link: 'text-primary underline-offset-4 hover:underline',
+        gray: 'bg-gray text-white',
       },
       size: {
         default: 'h-10 px-4 py-2',
@@ -39,7 +43,13 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
   },
 )
 Button.displayName = 'Button'
