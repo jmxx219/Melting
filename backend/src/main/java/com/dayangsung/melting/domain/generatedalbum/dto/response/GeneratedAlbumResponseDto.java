9b7b3697bf6cf2bbd15c4185e25971076b6e2c10
염규영 <yeomky98@naver.com>
2024-09-12 19:51:49 +0900

    feat: add response DTO for GeneratedAlbum
    Jira: S11P21A701-316

--- backend/src/main/java/com/dayangsung/melting/domain/generatedalbum/dto/response/GeneratedAlbumResponseDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/generatedalbum/dto/response/GeneratedAlbumResponseDto.java
@@ -1,5 +1,21 @@
 package com.dayangsung.melting.domain.generatedalbum.dto.response;
 
-public class GeneratedAlbumResponseDto {
-	
+import com.dayangsung.melting.domain.generatedalbum.entity.GeneratedAlbum;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Builder;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record GeneratedAlbumResponseDto(
+		String albumName,
+		String albumDescription
+) {
+	public static GeneratedAlbumResponseDto of(GeneratedAlbum album) {
+		return GeneratedAlbumResponseDto.builder()
+				.albumName(album.getAlbumName())
+				.albumDescription(album.getAlbumDescription())
+				.build();
+	}
 }
