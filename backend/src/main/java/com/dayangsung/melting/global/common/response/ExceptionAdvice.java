2c27f4798684cd4bcb0bfd4697ce5dd3e345f146
유승현 <fview@naver.com>
2024-09-13 13:51:57 +0900

    feat: swagger 설정
    Jira: S11P21A701-460

--- backend/src/main/java/com/dayangsung/melting/global/common/response/ExceptionAdvice.java
+++ backend/src/main/java/com/dayangsung/melting/global/common/response/ExceptionAdvice.java
@@ -1,17 +1,16 @@
-package com.dayangsung.melting.global.exception;
+package com.dayangsung.melting.global.common.response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.dayangsung.melting.global.common.response.ErrorResponse;
 import com.dayangsung.melting.global.common.response.enums.ErrorMessage;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.dayangsung.melting")
 public class ExceptionAdvice {
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
