51d9f466affe96a4a9fe1bd12dfc23503a389122
최은혜 <antlf1104@naver.com>
2024-09-15 20:52:59 +0900

    remove : OriginalSong 삭제
    Jira: S11P21A701-458

--- backend/src/main/java/com/dayangsung/melting/domain/originalsong/OriginalSongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dayangsung.melting.domain.originalsong;
-
-import com.dayangsung.melting.domain.originalsong.entity.OriginalSong;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OriginalSongRepository extends JpaRepository<OriginalSong, Long> {
-}
