80ce17b51f540220cdb5f4accfad5de8d83a8f20
최은혜 <antlf1104@naver.com>
2024-09-15 20:43:10 +0900

    refactor : ModelAttribute로 수정
    Jira: S11P21A701-458

--- backend/src/main/java/com/dayangsung/melting/domain/voice/controller/VoiceController.java
+++ backend/src/main/java/com/dayangsung/melting/domain/voice/controller/VoiceController.java
@@ -5,13 +5,14 @@ import com.dayangsung.melting.domain.voice.dto.request.VoiceCreateRequest;
 import com.dayangsung.melting.domain.voice.dto.response.VoiceCreateResponse;
 import com.dayangsung.melting.domain.voice.service.VoiceService;
 import com.dayangsung.melting.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +24,11 @@ public class VoiceController {
 
     private final VoiceService voiceService;
 
+
+    @Operation(summary = "사용자 보이스 등록 API")
     @PostMapping(value = "/me/voices", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<VoiceCreateResponse> addVoice(
-            @RequestBody VoiceCreateRequest voiceCreateRequest,
+            @ModelAttribute VoiceCreateRequest voiceCreateRequest,
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User) throws IOException {
         VoiceCreateResponse response = voiceService.addVoice(voiceCreateRequest, customOAuth2User.getId());
         return ApiResponse.ok(response);
