67c196a88879864f08d32e3a878498273be49b21
최은혜 <antlf1104@naver.com>
2024-09-18 15:18:50 +0900

    feat : 곡 앨범 커버이미지 수정
    Jira: S11P21A701-465

---
+++ backend/src/main/java/com/dayangsung/melting/domain/albumsong/entity/AlbumSong.java
@@ -0,0 +1,46 @@
+package com.dayangsung.melting.domain.albumsong.entity;
+
+import com.dayangsung.melting.domain.album.entity.Album;
+import com.dayangsung.melting.domain.song.entity.Song;
+import com.dayangsung.melting.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "album_song")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AlbumSong extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "album_id")
+	private Album album;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "song_id")
+	private Song song;
+
+	private int trackNumber;
+
+	@Builder
+	public AlbumSong(Album album, Song song, int trackNumber) {
+		this.album = album;
+		this.song = song;
+		this.trackNumber = trackNumber;
+	}
+}
--- backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
@@ -2,6 +2,7 @@ package com.dayangsung.melting.domain.song.service;
 
 import org.springframework.stereotype.Service;
 
+import com.dayangsung.melting.domain.albumsong.repository.AlbumSongRepository;
 import com.dayangsung.melting.domain.song.dto.response.SongDetailResponseDto;
 import com.dayangsung.melting.domain.song.entity.Song;
 import com.dayangsung.melting.domain.song.repository.SongRepository;
@@ -15,11 +16,13 @@ import lombok.extern.slf4j.Slf4j;
 public class SongService {
 
 	private final SongRepository songRepository;
+	private final AlbumSongRepository albumSongRepository;
 
 	public SongDetailResponseDto getSongDetail(Long songId) {
 		Song song = songRepository.findById(songId).orElseThrow(RuntimeException::new);
-		//Todo : album 수록곡에 따른 커버 이미지 불러오기
-		String albumImageUrl = "http://example.com/image.jpg";
+		String albumImageUrl = albumSongRepository.findLatestPublicAlbumCoverImageBySong(song)
+			.orElse("http://example.com/image.jpg");
+		//Todo : 기본 커버 이미지 추가
 		return SongDetailResponseDto.of(song, albumImageUrl);
 	}
 }
