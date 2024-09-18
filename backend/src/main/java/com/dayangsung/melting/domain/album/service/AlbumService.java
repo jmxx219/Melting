abbd5ec7c1ee70ab0c080df1786e967b51c72c53
gyuoo <yeomky98@naver.com>
2024-09-18 13:07:48 +0900

    comment: add comments to methods for clarity
    Jira: S11P21A701-316

--- backend/src/main/java/com/dayangsung/melting/domain/album/service/AlbumService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/service/AlbumService.java
@@ -5,6 +5,8 @@ import java.util.function.Supplier;
 
 import org.springframework.stereotype.Service;
 
+import com.dayangsung.melting.domain.album.dto.request.AlbumCreateRequestDto;
+import com.dayangsung.melting.domain.album.dto.request.AlbumUpdateRequestDto;
 import com.dayangsung.melting.domain.album.dto.response.AlbumResponseDto;
 import com.dayangsung.melting.domain.album.entity.Album;
 import com.dayangsung.melting.domain.album.repository.AlbumRepository;
@@ -17,17 +19,17 @@ public class AlbumService {
 
 	private final AlbumRepository albumRepository;
 
-	public List<Album> getAllAlbums() {
-		return albumRepository.findAll();
+	// 모든 앨범 조회
 	public List<AlbumResponseDto> getAllAlbums() {
 		return getAlbumsBy(albumRepository::findAll);
 	}
 
-	public List<Album> getPublicAlbums() {
-		return albumRepository.findByIsPublicTrue();
+	// 공개 설정되고, 삭제되지 않은 앨범 조회
 	public List<AlbumResponseDto> getPublicAndNotDeletedAlbums() {
 		return getAlbumsBy(albumRepository::findByIsPublicTrueAndIsDeletedFalse);
 	}
+
+	// 키워드 검색을 통한 앨범 조회
 	public List<AlbumResponseDto> searchAlbumsByKeyword(String keyword) {
 		if (keyword == null || keyword.trim().isEmpty()) {
 			return getPublicAndNotDeletedAlbums();
@@ -35,6 +37,7 @@ public class AlbumService {
 		return getAlbumsBy(() -> albumRepository.findByKeywordAndIsPublicTrueAndIsDeletedFalse(keyword));
 	}
 
+	// 공통 사용된 부분 메서드화
 	private List<AlbumResponseDto> getAlbumsBy(Supplier<List<Album>> albumSupplier) {
 		List<Album> albums = albumSupplier.get();
 		return albums.stream()
