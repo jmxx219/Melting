e67ec178a72475b437f5b8571c760de872053860
gyuoo <yeomky98@naver.com>
2024-09-18 12:53:47 +0900

    feat: add response DTOs for album details and listing
    Jira: S11P21A701-316

---
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/response/AlbumDetailsResponseDto.java
@@ -0,0 +1,35 @@
+package com.dayangsung.melting.domain.album.dto.response;
+
+import java.util.List;
+
+import com.dayangsung.melting.domain.album.entity.Album;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Builder;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record AlbumDetailsResponseDto(
+	Long albumId,
+	String albumName,
+	String category,
+	List<String> genres,
+	String albumDescription,
+	String albumCoverImage,
+	Long albumLiked,
+	Boolean isPublic
+) {
+	public static AlbumDetailsResponseDto of(Album album) {
+		return new AlbumDetailsResponseDto(
+			album.getGeneratedAlbumId(),
+			album.getAlbumName(),
+			album.getCategory(),
+			album.getGenres(),
+			album.getAlbumDescription(),
+			album.getAlbumCoverImage(),
+			album.getAlbumLiked(),
+			album.getIsPublic()
+		);
+	}
+}
