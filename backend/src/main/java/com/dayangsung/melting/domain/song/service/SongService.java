3c8cc94b69beb3fe7e1bd1b708202331640db981
최은혜 <antlf1104@naver.com>
2024-09-19 15:17:47 +0900

    feat : 곡 커버 기본이미지 추가
    Jira: S11P21A701-465

--- backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
@@ -6,6 +6,7 @@ import com.dayangsung.melting.domain.albumsong.repository.AlbumSongRepository;
 import com.dayangsung.melting.domain.song.dto.response.SongDetailResponseDto;
 import com.dayangsung.melting.domain.song.entity.Song;
 import com.dayangsung.melting.domain.song.repository.SongRepository;
+import com.dayangsung.melting.global.common.service.AwsS3Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +18,13 @@ public class SongService {
 
 	private final SongRepository songRepository;
 	private final AlbumSongRepository albumSongRepository;
+	private final AwsS3Service awsS3Service;
 
 	public SongDetailResponseDto getSongDetail(Long songId) {
 		Song song = songRepository.findById(songId).orElseThrow(RuntimeException::new);
 		String albumImageUrl = albumSongRepository.findLatestPublicAlbumCoverImageBySong(song)
-			.orElse("http://example.com/image.jpg");
-		//Todo : 기본 커버 이미지 추가
+			.orElse(awsS3Service.getDefaultProfileImageUrl());
+		//Todo: 스트리밍수 redis 추가 필요
 		return SongDetailResponseDto.of(song, albumImageUrl);
 	}
 }
