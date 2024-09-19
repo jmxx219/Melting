d98bf55106fd6553c35f8df24cc3d1bfae2668cb
최은혜 <antlf1104@naver.com>
2024-09-19 15:20:41 +0900

    rename : repository 어노테이션 삭제
    Jira: S11P21A701-465

--- backend/src/main/java/com/dayangsung/melting/domain/albumsong/repository/AlbumSongRepository.java
+++ backend/src/main/java/com/dayangsung/melting/domain/albumsong/repository/AlbumSongRepository.java
@@ -5,12 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.dayangsung.melting.domain.albumsong.entity.AlbumSong;
 import com.dayangsung.melting.domain.song.entity.Song;
 
-@Repository
 public interface AlbumSongRepository extends JpaRepository<AlbumSong, Long> {
 	@Query("SELECT als.album.albumCoverImage FROM AlbumSong AS als WHERE als.song = :song " +
 		"AND als.album.isPublic = true " +
