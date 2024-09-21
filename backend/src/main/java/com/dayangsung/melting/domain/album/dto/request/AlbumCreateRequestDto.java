4616726725aca35c851c61af02136d54c9b29ce9
gyuoo <yeomky98@naver.com>
2024-09-21 12:26:27 +0900

    refactor: update AlbumCreateRequestDto
    Jira: S11P21A701-316

--- backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/AlbumCreateRequestDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/AlbumCreateRequestDto.java
@@ -2,18 +2,20 @@ package com.dayangsung.melting.domain.album.dto.request;
 
 import java.util.List;
 
-import com.dayangsung.melting.domain.album.enums.AlbumCategory;
+import com.dayangsung.melting.domain.genre.entity.Genre;
+import com.dayangsung.melting.domain.hashtag.entity.Hashtag;
 import com.dayangsung.melting.domain.song.entity.Song;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+// TODO: 수정 필요
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AlbumCreateRequestDto(
 	String albumName,
-	AlbumCategory category,
-	List<String> genres,
-	String albumDescription,
 	String albumCoverImage,
-	List<Song> songs
+	String albumDescription,
+	List<Song> songs,
+	List<Hashtag> hashtags,
+	List<Genre> genres
 ) {
 }
