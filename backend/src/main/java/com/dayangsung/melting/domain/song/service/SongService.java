c69555c1f24426a90ae481765b91dada6075df22
최은혜 <antlf1104@naver.com>
2024-09-22 18:57:31 +0900

    feat: 스트리밍수 분산락 적용(Redisson)
    Jira: S11P21A701-465

--- backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
@@ -7,6 +7,7 @@ import com.dayangsung.melting.domain.song.dto.response.SongDetailsResponseDto;
 import com.dayangsung.melting.domain.song.entity.Song;
 import com.dayangsung.melting.domain.song.repository.SongRepository;
 import com.dayangsung.melting.global.common.service.AwsS3Service;
+import com.dayangsung.melting.global.redisson.DistributedLock;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +21,7 @@ public class SongService {
 	private final AwsS3Service awsS3Service;
 	private final RedisTemplate<String, Object> redisTemplate;
 
-	private static final String STREAMING_COUNT_KEY_PREFIX = "song:streaming:";
+	private static final String STREAMING_COUNT_KEY = "song:streaming:counts";
 
 	public SongDetailsResponseDto getSongDetails(Long songId) {
 		Song song = songRepository.findById(songId).orElseThrow(RuntimeException::new);
@@ -29,9 +30,17 @@ public class SongService {
 			albumCoverImage = song.getAlbum().getAlbumCoverImage();
 		}
 
-		String streamingCountKey = STREAMING_COUNT_KEY_PREFIX + songId;
-		redisTemplate.opsForValue().increment(streamingCountKey);
+		incrementStreamingCount(songId);
 
 		return SongDetailsResponseDto.of(song, albumCoverImage);
+	}
+
+	// Todo : 트랜잭션 적용 필요
+	@DistributedLock(value = "#songId")
+	public void incrementStreamingCount(Long songId) {
+		String key = STREAMING_COUNT_KEY;
+		Double streamingCount = redisTemplate.opsForZSet().incrementScore(key, songId.toString(), 1);
+		log.info("Incremented streaming count for song {}: {}", songId, streamingCount);
+
 	}
 }
