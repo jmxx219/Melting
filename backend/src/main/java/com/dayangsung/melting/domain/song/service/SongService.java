77c306d44f2b5dc399c0447a6265abc96f338b48
최은혜 <antlf1104@naver.com>
2024-09-22 16:08:15 +0900

    feat: 스트리밍수 증가 (redis)
    Jira: S11P21A701-465

--- backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
@@ -1,5 +1,6 @@
 package com.dayangsung.melting.domain.song.service;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.dayangsung.melting.domain.song.dto.response.SongDetailsResponseDto;
@@ -17,6 +18,9 @@ public class SongService {
 
 	private final SongRepository songRepository;
 	private final AwsS3Service awsS3Service;
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	private static final String STREAMING_COUNT_KEY_PREFIX = "song:streaming:";
 
 	public SongDetailsResponseDto getSongDetails(Long songId) {
 		Song song = songRepository.findById(songId).orElseThrow(RuntimeException::new);
@@ -24,7 +28,10 @@ public class SongService {
 		if (song.getAlbum() != null) {
 			albumCoverImage = song.getAlbum().getAlbumCoverImage();
 		}
-		//Todo: 스트리밍수 redis 추가 필요
+
+		String streamingCountKey = STREAMING_COUNT_KEY_PREFIX + songId;
+		redisTemplate.opsForValue().increment(streamingCountKey);
+
 		return SongDetailsResponseDto.of(song, albumCoverImage);
 	}
 }
