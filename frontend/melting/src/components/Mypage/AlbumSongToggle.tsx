1aff24b0a49101c3fdc97a8e450e2810f14090db
Jimin <jimmi219@naver.com>
2024-09-23 17:58:16 +0900

    feat: 정렬 드롭다운 추가
    Jira: S11P21A701-630

--- frontend/melting/src/components/Mypage/AlbumSongToggle.tsx
+++ frontend/melting/src/components/Mypage/AlbumSongToggle.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import AlbumList from '@/components/MyPage/AlbumList' // 앨범 리스트 컴포넌트
 import SongList from '@/components/MyPage/SongList' // 곡 리스트 컴포넌트
+import SortDropdown from '@/components/MyPage/SortDropdown'
 
 export default function AlbumSongToggle() {
   const [selectedTab, setSelectedTab] = useState<'album' | 'song'>('album')
@@ -14,7 +15,7 @@ export default function AlbumSongToggle() {
           {/* 앨범 버튼 */}
           <button
             onClick={() => setSelectedTab('album')}
-            className={`px-4 py-2 ${selectedTab === 'album' ? 'bg-primary-400 text-white' : 'bg-gray-200 text-black'}`}
+            className={`px-4 py-2 rounded-full ${selectedTab === 'album' ? 'bg-primary-400 text-white' : 'bg-gray-200 text-black'}`}
           >
             앨범
           </button>
@@ -22,23 +23,13 @@ export default function AlbumSongToggle() {
           {/* 곡 버튼 */}
           <button
             onClick={() => setSelectedTab('song')}
-            className={`px-4 py-2 ${selectedTab === 'song' ? 'bg-primary-400 text-white' : 'bg-gray-200 text-black'}`}
+            className={`px-4 py-2 rounded-full ${selectedTab === 'song' ? 'bg-primary-400 text-white' : 'bg-gray-200 text-black'}`}
           >
             곡
           </button>
         </div>
 
-        {/* 정렬 기준 선택 */}
-        <select
-          value={sortOption}
-          onChange={(e) =>
-            setSortOption(e.target.value as 'date' | 'popularity')
-          }
-          className="p-2 border border-gray-300 rounded"
-        >
-          <option value="date">최신순</option>
-          <option value="popularity">인기순</option>
-        </select>
+        <SortDropdown initialSort={sortOption} onSelect={setSortOption} />
       </div>
 
       {/* 결과 리스트: 선택된 탭에 따라 다르게 렌더링 */}
