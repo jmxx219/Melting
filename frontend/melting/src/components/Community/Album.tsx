2b36512e21471d92c59e1daccd4608f7b6053f4f
이보연 <btothey99@ewhain.net>
2024-09-23 17:20:00 +0900

    feat: 스테디 앨범 컴포넌트 구현
    Jira: S11P21A701-563

---
+++ frontend/melting/src/components/Community/Album.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { BestAlbum } from '@/types/album'
+import Play from '../icon/Play'
+
+interface AlbumProps {
+  album: BestAlbum
+}
+
+export default function Album({ album }: AlbumProps) {
+  const navigate = useNavigate()
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handlePlayClick = () => {
+    navigate(`/album/${album.albumId}/streaming`)
+  }
+
+  return (
+    <div className="w-[150px] h-full">
+      <div className="relative w-36 h-36">
+        <img
+          src={album.albumCoverImage}
+          alt={`${album.albumName}`}
+          className="w-full h-full object-cover rounded-lg"
+        />
+        <button
+          type="button"
+          onClick={handlePlayClick}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className={`absolute top-1 right-1 rounded-full shadow-md transition-colors duration-200 `}
+        >
+          <Play fill={isHovered ? '#ffaf25' : '#ffffff'} />
+        </button>
+      </div>
+      <div className="mt-2 text-left">
+        <h3 className="text-sm font-bold truncate">{album.albumName}</h3>
+        <p className="text-xs text-gray-500 truncate">{album.nickname}</p>
+      </div>
+    </div>
+  )
+}
--- frontend/melting/src/components/Home/Main.tsx
+++ frontend/melting/src/components/Home/Main.tsx
@@ -1,5 +1,6 @@
 import Greeting from './Greeting'
 import Intro from './Intro'
+import SteadyAlbum from './SteadyAlbums'
 
 export default function Main() {
   return (
@@ -11,8 +12,9 @@ export default function Main() {
         <Intro />
       </div>
       <div>
-        명반
-        <div>스테디 앨범</div>
+        <div>
+          <SteadyAlbum />
+        </div>
         <div>HOT 5 앨범</div>
         <div>이달의 앨범</div>
         <div>태그별 앨범 (비회원은 못봄)</div>
