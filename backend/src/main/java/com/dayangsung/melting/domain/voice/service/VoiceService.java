0cf7eac28ea7c814c94e3b233664396e902edf70
최은혜 <antlf1104@naver.com>
2024-09-23 23:55:20 +0900

    feat: 사용자 보이스 S3 등록 구현
    Jira: S11P21A701-458

--- backend/src/main/java/com/dayangsung/melting/domain/voice/service/VoiceService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/voice/service/VoiceService.java
@@ -1,44 +1,56 @@
 package com.dayangsung.melting.domain.voice.service;
 
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.dayangsung.melting.domain.member.entity.Member;
 import com.dayangsung.melting.domain.member.repository.MemberRepository;
-import com.dayangsung.melting.domain.originalsong.OriginalSongRepository;
 import com.dayangsung.melting.domain.originalsong.entity.OriginalSong;
+import com.dayangsung.melting.domain.originalsong.repository.OriginalSongRepository;
 import com.dayangsung.melting.domain.voice.dto.request.VoiceCreateRequest;
 import com.dayangsung.melting.domain.voice.dto.response.VoiceCreateResponse;
 import com.dayangsung.melting.domain.voice.entity.Voice;
 import com.dayangsung.melting.domain.voice.repository.VoiceRepository;
-import com.dayangsung.melting.global.common.service.LocalFileService;
-import java.io.IOException;
+import com.dayangsung.melting.global.common.service.AwsS3Service;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class VoiceService {
 
-    private final MemberRepository memberRepository;
-    private final OriginalSongRepository originalSongRepository;
-    private final VoiceRepository voiceRepository;
-    private final LocalFileService localFileService;
+	private final MemberRepository memberRepository;
+	private final OriginalSongRepository originalSongRepository;
+	private final VoiceRepository voiceRepository;
+	private final AwsS3Service awsS3Service;
 
+	@Transactional
+	public VoiceCreateResponse addVoice(VoiceCreateRequest request, Long memberId) throws IOException {
 
-    @Transactional
-    public VoiceCreateResponse addVoice(VoiceCreateRequest request, Long memberId) throws IOException {
-        Member member = memberRepository.findById(memberId).orElseThrow(RuntimeException::new);
-        OriginalSong originalSong = originalSongRepository.findById(request.originalSongId()).orElseThrow(RuntimeException::new);
+		Member member = memberRepository.findById(memberId).orElseThrow(RuntimeException::new);
+		OriginalSong originalSong = originalSongRepository.findById(request.originalSongId())
+			.orElseThrow(RuntimeException::new);
 
-        String voiceUrl = localFileService.saveAudioFile(request.voiceFile());
+		boolean voiceExists = voiceRepository.existsByMemberAndOriginalSong(member, originalSong);
 
-        Voice voice = request.toEntity(member, originalSong, voiceUrl);
+		String voiceUrl = awsS3Service.uploadVoice(request.voiceFile(), memberId, request.originalSongId());
+		Voice voice = request.toEntity(member, originalSong, voiceUrl);
 
-        voiceRepository.save(voice);
-        return VoiceCreateResponse.of(voice.getId());
+		voiceRepository.save(voice);
 
-    }
+		if (!voiceExists) {
+			member.increaseCoverCount();
+			memberRepository.save(member);
+		}
 
+		// Todo: AI 학습 및 배치 추가 필요
+
+		return VoiceCreateResponse.of(voice.getId());
+
+	}
 
 }
