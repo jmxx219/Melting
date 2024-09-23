336727b40efa6e23a1960ae42fdf32272081efa4
유승현 <fview@naver.com>
2024-09-23 17:33:02 +0900

    fix: 댓글 리스트 최신순 정렬
    Jira: S11P21A701-517

--- backend/src/main/java/com/dayangsung/melting/domain/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/dayangsung/melting/domain/comment/repository/CommentRepository.java
@@ -10,6 +10,6 @@ import com.dayangsung.melting.domain.comment.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-	@Query("SELECT c FROM Comment c WHERE c.album.id = :albumId AND c.isDeleted = false")
+	@Query("SELECT c FROM Comment c WHERE c.album.id = :albumId AND c.isDeleted = false ORDER BY c.createdAt DESC")
 	Slice<Comment> findByAlbumIdAndNotDeleted(@Param("albumId") Long albumId, Pageable pageable);
 }
