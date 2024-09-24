56509f8a624e75a6ad04f4dae0bb4c87273554a2
최은혜 <antlf1104@naver.com>
2024-09-24 13:46:09 +0900

    feat: S3 음성업로드 메서드 변경
    Jira: S11P21A701-458

--- backend/src/main/java/com/dayangsung/melting/domain/voice/service/VoiceService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/voice/service/VoiceService.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dayangsung.melting.domain.member.entity.Member;
 import com.dayangsung.melting.domain.member.repository.MemberRepository;
 import com.dayangsung.melting.domain.originalsong.entity.OriginalSong;
 import com.dayangsung.melting.domain.originalsong.repository.OriginalSongRepository;
-import com.dayangsung.melting.domain.voice.dto.request.VoiceCreateRequest;
-import com.dayangsung.melting.domain.voice.dto.response.VoiceCreateResponse;
+import com.dayangsung.melting.domain.voice.dto.response.VoiceCreateResponseDto;
 import com.dayangsung.melting.domain.voice.entity.Voice;
 import com.dayangsung.melting.domain.voice.repository.VoiceRepository;
 import com.dayangsung.melting.global.common.service.AwsS3Service;
@@ -29,16 +29,22 @@ public class VoiceService {
 	private final AwsS3Service awsS3Service;
 
 	@Transactional
-	public VoiceCreateResponse addVoice(VoiceCreateRequest request, Long memberId) throws IOException {
+	public VoiceCreateResponseDto addVoice(Long originalSongId, MultipartFile voiceFile, Long memberId) throws
+		IOException {
 
 		Member member = memberRepository.findById(memberId).orElseThrow(RuntimeException::new);
-		OriginalSong originalSong = originalSongRepository.findById(request.originalSongId())
+		OriginalSong originalSong = originalSongRepository.findById(originalSongId)
 			.orElseThrow(RuntimeException::new);
 
 		boolean voiceExists = voiceRepository.existsByMemberAndOriginalSong(member, originalSong);
 
-		String voiceUrl = awsS3Service.uploadVoice(request.voiceFile(), memberId, request.originalSongId());
-		Voice voice = request.toEntity(member, originalSong, voiceUrl);
+		String voiceUrl = awsS3Service.uploadVoice(voiceFile, member.getId(), originalSong.getId());
+
+		Voice voice = Voice.builder()
+			.member(member)
+			.originalSong(originalSong)
+			.voiceUrl(voiceUrl)
+			.build();
 
 		voiceRepository.save(voice);
 
@@ -49,7 +55,7 @@ public class VoiceService {
 
 		// Todo: AI 학습 및 배치 추가 필요
 
-		return VoiceCreateResponse.of(voice.getId());
+		return VoiceCreateResponseDto.of(voice.getId());
 
 	}
 
