1284e2e51d6fa4e1753389dc6990a2cc5510ed86
이보연 <btothey99@ewhain.net>
2024-09-24 14:45:58 +0900

    style: 로고 props 설정
    Jira: S11P21A701-563

--- frontend/melting/src/components/layout/Logo.tsx
+++ frontend/melting/src/components/layout/Logo.tsx
@@ -1,3 +1,14 @@
-export default function Logo() {
-  return <img src="/images/logo/logo.png" alt="로고" className="w-4 h-4" />
+interface LogoProps {
+  width?: number
+  height?: number
+}
+
+export default function Logo({ width = 4, height = 4 }: LogoProps) {
+  return (
+    <img
+      src="/images/logo/logo.png"
+      alt="로고"
+      className={`object-contain ${`w-${width} h-${height}`}`}
+    />
+  )
 }
