cf8633f551954189edd117154145cb8548e16813
DESKTOP-MMCQPV1\SSAFY <breakking@naver.com>
2024-09-24 13:49:09 +0900

    refact: 도메인 별 라우팅 컴포넌트 생성

---
+++ frontend/melting/src/routers/MyPageRoutes.tsx
@@ -0,0 +1,16 @@
+import LikedAlbumsAndSongs from '@/pages/MyPage/LikedAlbumsAndSongs'
+import MyAlbumsAndSongs from '@/pages/MyPage/MyAlbumsAndSongs'
+import MyPageMain from '@/pages/MyPage/MyPageMain'
+import ProfileEdit from '@/pages/MyPage/ProfileEdit'
+import { Route, Routes } from 'react-router-dom'
+
+const MyPageRoutes = () => (
+  <Routes>
+    <Route index element={<MyPageMain />} />
+    <Route path="profile/edit" element={<ProfileEdit />} />
+    <Route path="my" element={<MyAlbumsAndSongs />} />
+    <Route path="liked" element={<LikedAlbumsAndSongs />} />
+  </Routes>
+)
+
+export default MyPageRoutes
