6238c1d404ddf12ae0bba0d83681c693f47efe5b
손지민 <jimmi219@naver.com>
2024-09-25 17:02:19 +0900

    feat: 곡 조회 컴포넌트 구현
    Jira: S11P21A701-631

--- frontend/melting/src/components/MyPage/LikedSongList.tsx
+++ frontend/melting/src/components/MyPage/LikedSongList.tsx
@@ -7,13 +7,25 @@ interface LikedSongListProps {
 export default function LikedSongList({}: LikedSongListProps) {
   const songs = [
     {
-      song_id: 1,
-      album_cover_img_url: 'https://example.com/album_cover.jpg',
+      songId: 1,
+      albumCoverImgUrl:
+        'https://image.bugsm.co.kr/album/images/200/40955/4095501.jpg?version=20240307012526.0',
       artist: '아이유',
-      song_title: '좋아한 노래 제목 1',
-      nickname: '방배동 전상현',
-      execution_time: '3:14',
-      like_count: 12345,
+      songTitle: '좋아한 노래 제목 1입니다아아아',
+      nickname: '노원핵주먹안녕하세요노원핵주먹안녕하세요',
+      executionTime: '3:14',
+      likeCount: 123,
+      isLiked: true,
+    },
+    {
+      songId: 2,
+      albumCoverImgUrl:
+        'https://image.bugsm.co.kr/album/images/200/40955/4095501.jpg?version=20240307012526.0',
+      artist: '아이유',
+      songTitle: '좋아한 노래 제목 2',
+      nickname: '노원핵주먹',
+      executionTime: '3:14',
+      likeCount: 12345,
       isLiked: true,
     },
   ]
@@ -21,7 +33,11 @@ export default function LikedSongList({}: LikedSongListProps) {
   return (
     <div>
       {songs.map((song) => (
-        <LikedSongContent key={song.song_id} song={song} />
+        <LikedSongContent
+          key={song.songId}
+          song={song}
+          hasProfileImage={true}
+        />
       ))}
     </div>
   )
