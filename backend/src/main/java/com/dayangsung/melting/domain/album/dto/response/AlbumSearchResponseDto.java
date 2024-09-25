673ed654fe05fce5c305e0cc7c545c4bf2f507bb
gyuoo <yeomky98@naver.com>
2024-09-25 22:48:45 +0900

    refactor: add album id to AlbumSearchResponseDto
    Jira: S11P21A701-488

--- backend/src/main/java/com/dayangsung/melting/domain/album/dto/response/AlbumSearchResponseDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/response/AlbumSearchResponseDto.java
@@ -10,12 +10,14 @@ import lombok.Builder;
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AlbumSearchResponseDto(
-	String albumCoverImage,
-	String albumName,
-	String nickname
+		Long id,
+		String albumCoverImage,
+		String albumName,
+		String nickname
 ) {
 	public static AlbumSearchResponseDto of(Album album) {
 		return AlbumSearchResponseDto.builder()
+				.id(album.getId())
 				.albumCoverImage(album.getAlbumCoverImage())
 				.albumName(album.getAlbumName())
 				.nickname(album.getMember().getNickname())
