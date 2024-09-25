32c0002ebf9825f07bab5fb040ee33239d81ab7c
손지민 <jimmi219@naver.com>
2024-09-25 18:02:23 +0900

    rename: 대문자로 폴더 변경
    Jira: S11P21A701-631

--- frontend/melting/src/components/MyPage/SongContent.tsx
+++ frontend/melting/src/components/MyPage/SongContent.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Heart from '@/components/icon/Heart'
+import Heart from '@/components/Icon/Heart'
 import { Play, Crown } from 'lucide-react'
 
 interface LikedSongProps {
