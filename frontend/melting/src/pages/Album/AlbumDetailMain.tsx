df0d7a249a3bc1a41a0349bafbcb268e9dd9e6e6
DESKTOP-MMCQPV1\SSAFY <breakking@naver.com>
2024-09-26 13:19:21 +0900

    fix: import 에러 수정

--- frontend/melting/src/pages/Album/AlbumDetailMain.tsx
+++ frontend/melting/src/pages/Album/AlbumDetailMain.tsx
@@ -1,22 +1,24 @@
 import Layout from '@/components/Layout'
-import 
-type Props =import DefaultHeader from '@/components/Layout/DefaultHeader'
+import DefaultHeader from '@/components/Layout/DefaultHeader'
 import { ChevronLeft } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
- {}
 
-export default function AlbumDetailMain({}: Props) {
+export default function AlbumDetailMain() {
   //   const location = useLocation()
   //   const { albumId } = location.state || {}
-  const navigate = useNavigate();
-  return <Layout 
-    Header={<DefaultHeader
-        title={""}
-        buttonArea={<ChevronLeft color="#FFAF25" />}
-        move={() => {
-        navigate('/music')
-        }}
-  />}
-  children={}>
- </Layout>
+  const navigate = useNavigate()
+  return (
+    <Layout
+      Header={
+        <DefaultHeader
+          title={''}
+          buttonArea={<ChevronLeft color="#FFAF25" />}
+          move={() => {
+            navigate('/music')
+          }}
+        />
+      }
+      children={<></>}
+    ></Layout>
+  )
 }
