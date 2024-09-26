2237f075087b2a008e465d8acad46f7363078d49
이보연 <btothey99@ewhain.net>
2024-09-26 17:49:50 +0900

    fix: lint 오류 수정
    Jira: S11P21A701-593

--- frontend/melting/src/utils/OAuthUtil.ts
+++ frontend/melting/src/utils/OAuthUtil.ts
@@ -6,27 +6,19 @@ export default function OAuthRedirectHandler() {
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
-    const init = params.get('init')
+    const init = params.get('init') // 쿼리스트링에서 'init' 값 확인
 
-    // 쿠키 확인 로직
-    const checkCookie = (name: string) => {
-      return document.cookie
-        .split(';')
-        .some((item) => item.trim().startsWith(`${name}=`))
-    }
-
-    if (checkCookie('your_auth_cookie_name')) {
-      console.log('Auth cookie is set')
-      if (init === 'false') {
-        navigate('/signup')
-      } else if (init === 'true') {
-        navigate('/main')
-      }
+    if (init === 'false') {
+      // 신규 유저인 경우 회원가입 페이지로 이동
+      navigate('/signup')
+    } else if (init === 'true') {
+      // 기존 유저인 경우 메인 페이지로 이동
+      navigate('/main')
     } else {
-      console.error('Auth cookie is not set')
-      navigate('/login') // 로그인 실패로 간주하고 로그인 페이지로 리다이렉트
+      // 쿼리스트링 값이 없거나 올바르지 않은 경우 처리
+      console.error('Invalid redirect URL')
     }
   }, [navigate])
 
-  return null
+  return null // 리다이렉션 처리만 하고 렌더링할 UI는 없음
 }
