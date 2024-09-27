3c1ed492e5a1fd951c305f24e9f012c5d724c55c
이보연 <btothey99@ewhain.net>
2024-09-27 18:07:53 +0900

    style: 프로필 이미지 null 허용
    Jira: S11P21A701-593

--- frontend/melting/src/typeApis/members/data-contracts.ts
+++ frontend/melting/src/typeApis/members/data-contracts.ts
@@ -64,7 +64,7 @@ export type ReissueError = ErrorResponse
 
 export interface InitMemberInfoPayload {
   /** @format binary */
-  multipartFile: File
+  multipartFile?: File | null
   memberInitRequestDto?: MemberInitRequestDto
 }
 
