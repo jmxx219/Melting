84a2b492320f01771ed09c1f37aa58ca12e8e33d
유승현 <fview@naver.com>
2024-09-27 15:57:13 +0900

    fix: jwt 예외처리 로직 수정
    Jira: S11P21A701-536

---
+++ backend/src/main/java/com/dayangsung/melting/global/util/ResponseUtils.java
@@ -0,0 +1,44 @@
+package com.dayangsung.melting.global.util;
+
+import java.util.Map;
+
+import com.dayangsung.melting.global.common.response.ApiResponse;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ResponseUtils {
+	/**
+	 * 성공
+	 */
+	public static ApiResponse<Void> success() {
+		return success(null);
+	}
+
+	public static <T> ApiResponse<Map<String, T>> success(String key, T object) {
+		return success(Map.of(key, object));
+	}
+
+	public static <T> ApiResponse<T> success(T data) {
+		return ApiResponse.<T>builder()
+			.status("success")
+			.data(data)
+			.build();
+	}
+
+	/**
+	 * 에러
+	 */
+
+	public static ApiResponse<Void> error(String status, String message) {
+		return error("error", message);
+	}
+
+	public static <T> ApiResponse<T> error(T data, String message) {
+		return ApiResponse.<T>builder()
+			.status("error")
+			.data(data)
+			.build();
+	}
+}
