695d21c65f1f3d4c285e188aa491d64d67258ecb
이보연 <btothey99@ewhain.net>
2024-09-27 17:18:25 +0900

    feat: user 타입 정의
    Jira: S11P21A701-593

--- frontend/melting/src/types/user.ts
+++ frontend/melting/src/types/user.ts
@@ -6,3 +6,74 @@ export interface user {
   gender: string
   profileImage: string
 }
+
+export interface ErrorResponse {
+  errorMessage?:
+    | 'MEMBER_NOT_FOUND'
+    | 'DUPLICATE_NICKNAME'
+    | 'INCORRECT_IMAGE_EXTENSION'
+    | 'MEMBER_BAD_REQUEST'
+    | 'ALBUM_NOT_FOUND'
+    | 'INVALID_SORT_CRITERIA'
+    | 'INVALID_SONG_COUNT'
+    | 'SEARCH_QUERY_TOO_SHORT'
+    | 'ALBUM_NAME_BLANK_ERROR'
+    | 'ALBUM_COVER_IMAGE_BLANK_ERROR'
+    | 'ALBUM_SONGS_EMPTY_ERROR'
+}
+
+export interface ApiResponseString {
+  status?: string
+  data?: string
+  errorMessage?: string
+}
+
+export interface MemberInitRequestDto {
+  nick_name?: string
+  gender?: string
+}
+
+export interface ApiResponseMemberResponseDto {
+  status?: string
+  data?: MemberResponseDto
+  errorMessage?: string
+}
+
+export interface MemberResponseDto {
+  nickname?: string
+  profile_image_url?: string
+}
+
+export interface ApiResponseBoolean {
+  status?: string
+  data?: boolean
+  errorMessage?: string
+}
+
+export interface ApiResponseVoid {
+  status?: string
+  data?: object
+  errorMessage?: string
+}
+
+export type ReissueData = ApiResponseString
+
+export type ReissueError = ErrorResponse
+
+export interface InitMemberInfoPayload {
+  /** @format binary */
+  multipartFile?: File | null
+  memberInitRequestDto?: MemberInitRequestDto
+}
+
+export type InitMemberInfoData = ApiResponseMemberResponseDto
+
+export type InitMemberInfoError = ErrorResponse
+
+export type ValidateNicknameData = ApiResponseBoolean
+
+export type ValidateNicknameError = ErrorResponse
+
+export type LogoutData = ApiResponseVoid
+
+export type LogoutError = ErrorResponse
