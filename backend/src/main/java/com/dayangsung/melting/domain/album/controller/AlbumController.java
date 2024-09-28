7d9e33cc2be8e50274691d2bc268e866da720fff
유승현 <fview@naver.com>
2024-09-28 23:23:43 +0900

    feat: 앨범 리스트 최신순 조회 api
    Jira: S11P21A701-533

--- backend/src/main/java/com/dayangsung/melting/domain/album/controller/AlbumController.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/controller/AlbumController.java
@@ -1,5 +1,6 @@
 package com.dayangsung.melting.domain.album.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.dayangsung.melting.domain.album.dto.request.AlbumCreateRequestDto;
 import com.dayangsung.melting.domain.album.dto.request.AlbumUpdateRequestDto;
 import com.dayangsung.melting.domain.album.dto.response.AlbumDetailsResponseDto;
+import com.dayangsung.melting.domain.album.dto.response.AlbumSearchResponseDto;
 import com.dayangsung.melting.domain.album.service.AlbumService;
 import com.dayangsung.melting.domain.auth.CustomOAuth2User;
 import com.dayangsung.melting.global.common.response.ApiResponse;
@@ -26,6 +29,15 @@ import lombok.RequiredArgsConstructor;
 public class AlbumController {
 
 	private final AlbumService albumService;
+
+	@GetMapping
+	public ApiResponse<Page<AlbumSearchResponseDto>> searchAlbum(
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size) {
+
+		Page<AlbumSearchResponseDto> albumPage = albumService.searchAlbum(page, size);
+		return ApiResponse.ok(albumPage);
+	}
 
 	@PostMapping
 	public ApiResponse<AlbumDetailsResponseDto> createAlbum(
--- backend/src/main/java/com/dayangsung/melting/domain/album/repository/AlbumRepository.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/repository/AlbumRepository.java
@@ -1,8 +1,12 @@
 package com.dayangsung.melting.domain.album.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dayangsung.melting.domain.album.entity.Album;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+	Page<Album> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
