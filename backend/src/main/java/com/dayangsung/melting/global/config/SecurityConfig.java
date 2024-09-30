eed324f1ce2890d1556973a21756a7d361821dbd
유승현 <fview@naver.com>
2024-09-30 14:39:10 +0900

    remove: 불필요한 코드 제거
    Jira: S11P21A701-519

--- backend/src/main/java/com/dayangsung/melting/global/config/SecurityConfig.java
+++ backend/src/main/java/com/dayangsung/melting/global/config/SecurityConfig.java
@@ -66,8 +66,7 @@ public class SecurityConfig {
 					.requestMatchers(
 						"/swagger-ui/**",
 						"/v3/api-docs/**",
-						"/oauth2/**",
-						"/api/v1/**"
+						"/oauth2/**"
 					)
 					.permitAll()
 					.anyRequest()
