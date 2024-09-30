e8478a5ffd2f25a9629355dabe0a05813af59f20
유승현 <fview@naver.com>
2024-09-30 14:42:53 +0900

    refactor: dto 변수 형식 카멜로 변경
    Jira: S11P21A701-536

--- backend/src/main/java/com/dayangsung/melting/domain/comment/dto/request/CommentRequestDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/comment/dto/request/CommentRequestDto.java
@@ -1,9 +1,5 @@
 package com.dayangsung.melting.domain.comment.dto.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CommentRequestDto(
 	String content
 ) {
--- backend/src/main/java/com/dayangsung/melting/domain/comment/dto/response/CommentResponseDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/comment/dto/response/CommentResponseDto.java
@@ -3,13 +3,10 @@ package com.dayangsung.melting.domain.comment.dto.response;
 import java.time.LocalDateTime;
 
 import com.dayangsung.melting.domain.comment.entity.Comment;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.Builder;
 
 @Builder
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CommentResponseDto(
 	Long commentId,
 	String writerProfileImage,
