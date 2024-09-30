3594a1edbf230b533ae32422ab70a8491d05f290
유승현 <fview@naver.com>
2024-09-30 13:01:39 +0900

    fix: 충돌 해결
    Jira: S11P21A701-533

--- backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/song/service/SongService.java
@@ -6,6 +6,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.dayangsung.melting.domain.likes.service.LikesService;
+import com.dayangsung.melting.domain.member.entity.Member;
+import com.dayangsung.melting.domain.member.repository.MemberRepository;
 import com.dayangsung.melting.domain.song.dto.response.SongDetailsResponseDto;
 import com.dayangsung.melting.domain.song.entity.Song;
 import com.dayangsung.melting.domain.song.repository.SongRepository;
@@ -26,16 +28,21 @@ public class SongService {
 	private final RedisTemplate<String, Object> redisTemplate;
 
 	private static final String STREAMING_COUNT_KEY = "song:streaming:counts";
+	private final MemberRepository memberRepository;
 
-	public SongDetailsResponseDto getSongDetails(Long songId) {
+	public SongDetailsResponseDto getSongDetails(Long songId, String email) {
 		Song song = songRepository.findById(songId).orElseThrow(RuntimeException::new);
+		Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
+		String albumCoverImage = awsS3Service.getDefaultSongCoverImageUrl();
+
 		String albumCoverImageUrl = awsS3Service.getDefaultSongCoverImageUrl();
 		if (song.getAlbum() != null) {
 			albumCoverImageUrl = song.getAlbum().getAlbumCoverImageUrl();
 		}
-		incrementStreamingCount(songId);
-		return SongDetailsResponseDto.of(song, albumCoverImageUrl, likesService.getSongLikesCount(songId));
+		incrementStreamingCount(song.getId());
 
+		boolean isLiked = likesService.isLikedBySongAndMember(song.getId(), member.getId());
+		return SongDetailsResponseDto.of(song, albumCoverImage, isLiked, likesService.getSongLikesCount(song.getId()));
 	}
 
 	// Todo : 트랜잭션 적용 필요
