c29f15c04a9ec6cb3595574b8f4735afa2f78576
DESKTOP-MMCQPV1\SSAFY <breakking@naver.com>
2024-10-02 09:42:49 +0900

    fix: 로컬스토리지로 isLogin 관리

--- frontend/melting/src/routers/ProtectRouter.tsx
+++ frontend/melting/src/routers/ProtectRouter.tsx
@@ -1,8 +1,10 @@
-import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '@/contexts/AuthContext'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedRoute = () => {
   const { isLogin } = useAuth()
+
+  console.log(isLogin)
 
   if (!isLogin) {
     return <Navigate to="/login" replace /> // If not logged in, redirect to login page
