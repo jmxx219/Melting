cbe59a7162c777f9ef8711938d99560c9210eec7
염규영 <yeomky98@naver.com>
2024-10-02 19:33:58 +0900

    refactor: remove unnecessary annotation
    Jira: S11P21A701-707

--- backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/openai/AiCoverImageRequestDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/openai/AiCoverImageRequestDto.java
@@ -3,10 +3,7 @@ package com.dayangsung.melting.domain.album.dto.request.openai;
 import java.util.List;
 
 import com.dayangsung.melting.domain.originalsong.dto.response.OriginalSongAiResponseDto;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AiCoverImageRequestDto(
 		List<OriginalSongAiResponseDto> songs
 ) {
--- backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/openai/AiDescriptionRequestDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/openai/AiDescriptionRequestDto.java
@@ -5,10 +5,7 @@ import java.util.List;
 import com.dayangsung.melting.domain.genre.entity.Genre;
 import com.dayangsung.melting.domain.hashtag.entity.Hashtag;
 import com.dayangsung.melting.domain.song.dto.SongResultDto;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AiDescriptionRequestDto (
 		List<SongResultDto> songs,
 		List<Hashtag> hashtags,
