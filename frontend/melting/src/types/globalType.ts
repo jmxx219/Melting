ea77c82358f02c4b277d8865e6847fb2aa608f2f
이보연 <btothey99@ewhain.net>
2024-10-02 17:22:16 +0900

    type: 전역 상수 정의
    Jira: S11P21A701-602

--- frontend/melting/src/types/globalType.ts
+++ frontend/melting/src/types/globalType.ts
@@ -1,3 +1,5 @@
+import { FullRequestParams } from '@/typeApis/http-client.ts'
+
 export interface SVGIconProps {
   width?: number
   height?: number
@@ -10,12 +12,20 @@ export interface ErrorResponse {
     | 'DUPLICATE_NICKNAME'
     | 'INCORRECT_IMAGE_EXTENSION'
     | 'MEMBER_BAD_REQUEST'
+    | 'MEMBER_HASHTAG_FULL'
+    | 'MEMBER_HASHTAG_EMPTY'
+    | 'MEMBER_HASHTAG_EXIST'
+    | 'SONG_NOT_FOUND'
+    | 'SONG_ALREADY_INCLUDED'
+    | 'GENRE_NOT_FOUND'
+    | 'HASHTAG_NOT_FOUND'
     | 'ALBUM_NOT_FOUND'
     | 'INVALID_SORT_CRITERIA'
     | 'INVALID_SONG_COUNT'
     | 'SEARCH_QUERY_TOO_SHORT'
     | 'ALBUM_NAME_BLANK_ERROR'
     | 'ALBUM_COVER_IMAGE_BLANK_ERROR'
+    | 'INVALID_FILE_TYPE'
     | 'ALBUM_SONGS_EMPTY_ERROR'
 }
 
@@ -31,3 +41,8 @@ export interface ApiResponseString {
   data?: string
   errorMessage?: string
 }
+
+export type RequestParams = Omit<
+  FullRequestParams,
+  'body' | 'method' | 'query' | 'path'
+>
