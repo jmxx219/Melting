75b512ca41a96411475f21b13b766ab418252803
염규영 <yeomky98@naver.com>
2024-10-02 10:41:48 +0900

    refactor: replace loop with Stream API in findAll method
    Jira: S11P21A701-650

--- backend/src/main/java/com/dayangsung/melting/domain/genre/service/GenreService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/genre/service/GenreService.java
@@ -1,16 +1,13 @@
 package com.dayangsung.melting.domain.genre.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dayangsung.melting.domain.genre.dto.response.GenreResponseDto;
-import com.dayangsung.melting.domain.genre.entity.Genre;
 import com.dayangsung.melting.domain.genre.repository.GenreRepository;
 
-import jakarta.persistence.Table;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -21,11 +18,9 @@ public class GenreService {
 
 	@Transactional(readOnly = true)
 	public List<GenreResponseDto> findAll() {
-		List<Genre> result = genreRepository.findAll();
-		List<GenreResponseDto> resultToDto = new ArrayList<>();
-		for (Genre genre : result) {
-			resultToDto.add(GenreResponseDto.of(genre));
-		}
-		return resultToDto;
+		return genreRepository.findAll().stream()
+				.map(GenreResponseDto::of)
+				.toList();
 	}
+
 }
