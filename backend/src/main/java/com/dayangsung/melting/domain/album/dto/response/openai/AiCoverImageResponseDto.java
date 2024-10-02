a782ee3583aeb17fd464a05440e4b3bade3afb1a
염규영 <yeomky98@naver.com>
2024-10-02 19:39:26 +0900

    refactor: change DTOs to records and add JSON property annotations
    Jira: S11P21A701-707

--- backend/src/main/java/com/dayangsung/melting/domain/album/dto/response/openai/AiCoverImageResponseDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/response/openai/AiCoverImageResponseDto.java
@@ -4,14 +4,7 @@ package com.dayangsung.melting.domain.album.dto.response.openai;
 
 import java.util.List;
 
-import lombok.Data;
-
-/**
- * OpenAI API 형식과 일치하도록 작성하였음
- */
-
-@Data
-public class AiCoverImageResponseDto {
-	long created;
-	List<ImageObject> data;
+public record AiCoverImageResponseDto(
+		long created,
+		List<ImageObject> data) {
 }
