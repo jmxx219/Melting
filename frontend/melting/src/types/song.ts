d97f9a362d5e4ec7fc3e5bdd841926051f956c31
이보연 <btothey99@ewhain.net>
2024-10-03 19:18:16 +0900

    feat: song 타입 구현
    Jira: S11P21A701-601

--- frontend/melting/src/types/song.ts
+++ frontend/melting/src/types/song.ts
@@ -1,12 +1,15 @@
 import { CoverType } from './constType'
+import { ErrorResponse } from '@/types/globalType.ts'
 
 export type Song = {
   songId: number
-  albumCoverImgUrl: string
   songTitle: string
-  nickname?: string
   artist: string
-  songType?: CoverType
+  albumCoverImageUrl: string
+  nickname: string
+  songType: CoverType
+  meltingSongId: number | null
+  aiCoverSongId: number | null
 }
 
 export interface SongListProps {
@@ -25,3 +28,38 @@ export type LikedSongType = {
   likeCount: number
   isLiked: boolean
 }
+
+export interface SongSearchResponseDto {
+  originalSongTitle: string
+  originalSongArtist: string
+  albumCoverImage: string
+  /** @format int64 */
+  meltingSongId?: number
+  /** @format int64 */
+  aiCoverSongId?: number
+}
+
+export interface SongSearchPageResponseDto {
+  songSearchResponseDtoList?: SongSearchResponseDto[]
+  isLast?: boolean
+  /** @format int32 */
+  pageNumber?: number
+  /** @format int32 */
+  pageSize?: number
+  /** @format int32 */
+  totalPages?: number
+  /** @format int64 */
+  totalElements?: number
+  /** @format int32 */
+  numberOfElements?: number
+}
+
+export interface ApiResponseSongSearchPageResponseDto {
+  status?: string
+  data?: SongSearchPageResponseDto
+  errorMessage?: string
+}
+
+export type GetSongsForAlbumCreationData = ApiResponseSongSearchPageResponseDto
+
+export type GetSongsForAlbumCreationError = ErrorResponse
