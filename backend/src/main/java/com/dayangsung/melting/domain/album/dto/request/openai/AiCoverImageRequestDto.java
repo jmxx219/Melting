6c1737ec6b147e8a64622ebf96c61c2e20db4d4e
gyuoo <yeomky98@naver.com>
2024-10-03 13:39:33 +0900

    feat: add methods to convert ID to entity
    Jira: S11P21A701-707

--- backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/openai/AiCoverImageRequestDto.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/dto/request/openai/AiCoverImageRequestDto.java
@@ -2,9 +2,7 @@ package com.dayangsung.melting.domain.album.dto.request.openai;
 
 import java.util.List;
 
-import com.dayangsung.melting.domain.originalsong.dto.response.OriginalSongAiResponseDto;
-
 public record AiCoverImageRequestDto(
-		List<OriginalSongAiResponseDto> songs
+		List<Long> songs // Original song ID
 ) {
 }
