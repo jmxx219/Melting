d09920ffde9e304aa9d66161ff386a268b7fe82c
유승현 <fview@naver.com>
2024-10-04 15:54:49 +0900

    fix: 하드코딩 수정
    Jira: S11P21A701-659

--- backend/src/main/java/com/dayangsung/melting/domain/album/controller/AlbumController.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/controller/AlbumController.java
@@ -137,8 +137,9 @@ public class AlbumController {
 	}
 
 	@PostMapping("/{albumId}/likes")
-	public ApiResponse<Integer> addAlbumLikes(@PathVariable Long albumId) {
-		Integer albumLikesCount = albumService.increaseAlbumLikes(albumId, "ssafy@ssafy.com");
+	public ApiResponse<Integer> addAlbumLikes(@PathVariable Long albumId,
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+		Integer albumLikesCount = albumService.increaseAlbumLikes(albumId, customOAuth2User.getName());
 		return ApiResponse.ok(albumLikesCount);
 	}
 
