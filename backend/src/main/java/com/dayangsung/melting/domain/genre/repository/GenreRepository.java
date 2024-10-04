5424187c00ec1259bf7420597b5b76e465862bf7
유승현 <fview@naver.com>
2024-10-04 16:53:27 +0900

    refactor: 앨범 생성 시 장르 요청 타입 변경
    Jira: S11P21A701-536

--- backend/src/main/java/com/dayangsung/melting/domain/genre/repository/GenreRepository.java
+++ backend/src/main/java/com/dayangsung/melting/domain/genre/repository/GenreRepository.java
@@ -1,8 +1,11 @@
 package com.dayangsung.melting.domain.genre.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dayangsung.melting.domain.genre.entity.Genre;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+	Optional<Genre> findByContent(String content);
 }
