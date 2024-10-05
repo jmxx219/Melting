eab09e2e14a5cfc036d300d94c144ca9a735c97a
유승현 <fview@naver.com>
2024-10-05 13:42:27 +0900

    feat: 해시태그 검색 api
    Jira: S11P21A701-734

--- backend/src/main/java/com/dayangsung/melting/domain/genre/service/GenreService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/genre/service/GenreService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dayangsung.melting.domain.album.entity.Album;
 import com.dayangsung.melting.domain.genre.dto.response.GenreResponseDto;
+import com.dayangsung.melting.domain.genre.entity.AlbumGenre;
 import com.dayangsung.melting.domain.genre.entity.Genre;
+import com.dayangsung.melting.domain.genre.repository.AlbumGenreRepository;
 import com.dayangsung.melting.domain.genre.repository.GenreRepository;
 import com.dayangsung.melting.global.common.enums.ErrorMessage;
 import com.dayangsung.melting.global.exception.BusinessException;
@@ -18,17 +21,27 @@ import lombok.RequiredArgsConstructor;
 public class GenreService {
 
 	private final GenreRepository genreRepository;
+	private final AlbumGenreRepository albumGenreRepository;
 
 	@Transactional(readOnly = true)
 	public List<GenreResponseDto> findAll() {
 		return genreRepository.findAll().stream()
-				.map(GenreResponseDto::of)
-				.toList();
+			.map(GenreResponseDto::of)
+			.toList();
 	}
 
 	@Transactional(readOnly = true)
 	public List<Genre> idListToGenreList(List<Long> idList) {
 		return idList.stream().map(genreId -> genreRepository.findById(genreId)
 			.orElseThrow(() -> new BusinessException(ErrorMessage.GENRE_NOT_FOUND))).toList();
+	}
+
+	@Transactional
+	public void addAlbumGenre(Album album, String content) {
+		Genre genre = genreRepository.findByContent(content)
+			.orElseThrow(() -> new BusinessException(ErrorMessage.GENRE_NOT_FOUND));
+		AlbumGenre albumGenre = AlbumGenre.builder().album(album).genre(genre).build();
+		albumGenre = albumGenreRepository.save(albumGenre);
+		album.addGenre(albumGenre);
 	}
 }
