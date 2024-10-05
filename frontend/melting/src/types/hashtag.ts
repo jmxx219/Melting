353e4df0126671d9a8d4bcc193f2891457572bc7
이보연 <btothey99@ewhain.net>
2024-10-05 19:20:53 +0900

    feat: 해시태그 검색 api 구현
    Jira: S11P21A701-605

---
+++ frontend/melting/src/types/hashtag.ts
@@ -0,0 +1,30 @@
+import { ErrorResponse } from '@/types/globalType.ts'
+
+export interface HashtagResponseDto {
+  /** @format int64 */
+  id?: number
+  content?: string
+}
+export interface HashtagPageResponseDto {
+  hashtags?: HashtagResponseDto[]
+  isLast?: boolean
+  /** @format int32 */
+  pageNumber?: number
+  /** @format int32 */
+  pageSize?: number
+  /** @format int32 */
+  totalPages?: number
+  /** @format int64 */
+  totalElements?: number
+  /** @format int32 */
+  numberOfElements?: number
+}
+export interface ApiResponseHashtagPageResponseDto {
+  status?: string
+  data?: HashtagPageResponseDto
+  errorMessage?: string
+}
+
+export type SearchHashtagsData = ApiResponseHashtagPageResponseDto
+
+export type SearchHashtagsError = ErrorResponse
