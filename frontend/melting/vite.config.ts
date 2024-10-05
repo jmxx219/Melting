c681f381fe05a3965d192d4f1fedf21371f57ba3
DESKTOP-MMCQPV1\SSAFY <breakking@naver.com>
2024-10-05 22:41:36 +0900

    fix: 재녹음 시 녹음 페이지 이동

--- frontend/melting/vite.config.ts
+++ frontend/melting/vite.config.ts
@@ -1,18 +1,18 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
-// import fs from 'fs'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync(path.resolve(__dirname, './mkcert/key.pem')),
-  //     cert: fs.readFileSync(path.resolve(__dirname, './mkcert/cert.pem')),
-  //   },
-  //   host: 'localhost',
-  //   port: 5173, // 원하는 포트 번호 설정 가능
-  // },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './mkcert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './mkcert/cert.pem')),
+    },
+    host: 'localhost',
+    port: 5173, // 원하는 포트 번호 설정 가능
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
