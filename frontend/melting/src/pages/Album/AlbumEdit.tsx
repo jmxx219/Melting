80643b7bc9e60d4c82eb577c443074dbbc5677c4
이보연 <btothey99@ewhain.net>
2024-10-05 11:42:42 +0900

    feat: 앨범 수정 페이지 구현
    Jira: S11P21A701-729

---
+++ frontend/melting/src/pages/Album/AlbumEdit.tsx
@@ -0,0 +1,13 @@
+import AlbumEdit from '@/components/Album/Modify/AlbumEdit'
+import Layout from '@/components/Layout'
+import TitleHeader from '@/components/Layout/TitleHeader'
+
+export default function AlbumEditPage() {
+  return (
+    <Layout
+      Header={<TitleHeader titles={['앨범 정보를', '수정해주세요']} />}
+      children={<AlbumEdit />}
+      isMainCenter={true}
+    />
+  )
+}
