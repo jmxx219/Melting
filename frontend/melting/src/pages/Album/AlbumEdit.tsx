a43b30ba98656c51404ded42a3d44ba668caa69f
이보연 <btothey99@ewhain.net>
2024-10-05 20:37:03 +0900

    fix: lint 수정
    Jira: S11P21A701-729

--- frontend/melting/src/pages/Album/AlbumEdit.tsx
+++ frontend/melting/src/pages/Album/AlbumEdit.tsx
@@ -1,13 +1,14 @@
-import AlbumEdit from '@/components/Album/Modify/AlbumEdit'
-import Layout from '@/components/Layout'
-import TitleHeader from '@/components/Layout/TitleHeader'
+// import AlbumEdit from '@/components/Album/Modify/AlbumEdit'
+// import Layout from '@/components/Layout'
+// import TitleHeader from '@/components/Layout/TitleHeader'
 
 export default function AlbumEditPage() {
   return (
-    <Layout
-      Header={<TitleHeader titles={['앨범 정보를', '수정해주세요']} />}
-      children={<AlbumEdit />}
-      isMainCenter={true}
-    />
+    <></>
+    // <Layout
+    //   Header={<TitleHeader titles={['앨범 정보를', '수정해주세요']} />}
+    //   children={<AlbumEdit />}
+    //   isMainCenter={true}
+    // />
   )
 }
