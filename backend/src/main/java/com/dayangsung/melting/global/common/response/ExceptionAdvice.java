2d88c212936658342a5582eb8332ccaffb195dcf
Jimin <jimmi219@naver.com>
2024-10-06 00:01:22 +0900

    feat: error webhook 설정

--- backend/src/main/java/com/dayangsung/melting/global/common/response/ExceptionAdvice.java
+++ backend/src/main/java/com/dayangsung/melting/global/common/response/ExceptionAdvice.java
@@ -1,11 +1,13 @@
 package com.dayangsung.melting.global.common.response;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.dayangsung.melting.global.common.enums.ErrorMessage;
+import com.dayangsung.melting.global.exception.MeltingException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,5 +21,15 @@ public class ExceptionAdvice {
 		return ErrorResponse.builder()
 			.errorMessage(ErrorMessage.valueOf(e.toString()))
 			.build();
+	}
+
+	@ExceptionHandler(value = MeltingException.class)
+	public ResponseEntity<ErrorResponse> handleMeltingException(MeltingException e) {
+		ErrorMessage errorMessage = e.getErrorMessage();
+		log.info("MeltingException {}", errorMessage.getErrorMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ErrorResponse.builder()
+				.errorMessage(ErrorMessage.valueOf(e.toString()))
+				.build());
 	}
 }
