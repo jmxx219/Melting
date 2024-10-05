8f8c74ee10108375f98f226be451324680257e94
이보연 <btothey99@ewhain.net>
2024-10-05 19:26:10 +0900

    feat: 해시태그 검색 api 구현
    Jira: S11P21A701-605

--- frontend/melting/src/apis/hashtagApi.ts
+++ frontend/melting/src/apis/hashtagApi.ts
@@ -1,4 +1,9 @@
 import { createAxiosInstance, createApi, ApiResponse } from './axiosInstance'
+import {
+  HashtagPageResponseDto,
+  SearchHashtagsData,
+  SearchHashtagsError,
+} from '@/types/hashtag.ts'
 
 const instance = createAxiosInstance('hashtags')
 const api = createApi<ApiResponse>(instance)
@@ -8,22 +13,19 @@ export const hashtagApi = {
     keyword: string | null,
     page?: number,
     size?: number,
-  ): Promise<SearchHashtagsData> => {
+  ): Promise<HashtagPageResponseDto> => {
     try {
-      const response = await instance.get<SearchHashtagsData>(
-        '/api/v1/hashtags',
-        {
-          params: {
-            keyword,
-            page,
-            size,
-          },
+      const response = await api.get<SearchHashtagsData>('', {
+        params: {
+          keyword,
+          page,
+          size,
         },
-      )
-      return response.data
+      })
+      return response.data as HashtagPageResponseDto
     } catch (error) {
       console.error('해시태그 조회 중 오류 발생:', error)
-      throw error // 오류 처리
+      throw error as SearchHashtagsError // 오류 처리
     }
   },
 }
