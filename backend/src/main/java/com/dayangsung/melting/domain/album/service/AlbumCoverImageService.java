6df7a925df57c3dbe2f62a56a9cd5a569a3bf68f
gyuoo <yeomky98@naver.com>
2024-10-06 16:24:24 +0900

    refactor: remove unused imports
    Jira: S11P21A701-711

--- backend/src/main/java/com/dayangsung/melting/domain/album/service/AlbumCoverImageService.java
+++ backend/src/main/java/com/dayangsung/melting/domain/album/service/AlbumCoverImageService.java
@@ -2,7 +2,6 @@ package com.dayangsung.melting.domain.album.service;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
