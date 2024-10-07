90402a03a6418decfe63974ee06de85aab5c6ec8
Jimin <jimmi219@naver.com>
2024-10-07 17:12:20 +0900

    feat: 엘라스틱 해시태그 검색 구현

---
+++ backend/src/main/java/com/dayangsung/melting/domain/elasticsearch/document/HashtagDocument.java
@@ -0,0 +1,38 @@
+package com.dayangsung.melting.domain.elasticsearch.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import com.dayangsung.melting.domain.hashtag.entity.Hashtag;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(indexName = "hashtags")
+public class HashtagDocument {
+	@Id
+	@Field(name = "id", type = FieldType.Keyword)
+	private Long id;
+
+	@Field(type = FieldType.Keyword)
+	private String content;
+
+	@Builder
+	private HashtagDocument(Long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public static HashtagDocument createHashTagDocument(Hashtag hashtag) {
+		return HashtagDocument.builder()
+			.id(hashtag.getId())
+			.content(hashtag.getContent())
+			.build();
+	}
+}
---
+++ backend/src/main/java/com/dayangsung/melting/domain/elasticsearch/service/HashtagSearchService.java
@@ -0,0 +1,45 @@
+package com.dayangsung.melting.domain.elasticsearch.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dayangsung.melting.domain.elasticsearch.document.HashtagDocument;
+import com.dayangsung.melting.domain.elasticsearch.repository.HashtagDocumentRepository;
+import com.dayangsung.melting.domain.hashtag.dto.response.HashtagResponseDto;
+import com.dayangsung.melting.domain.hashtag.entity.Hashtag;
+import com.dayangsung.melting.domain.hashtag.repository.HashtagRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class HashtagSearchService {
+
+	private final HashtagRepository hashtagRepository;
+	private final HashtagDocumentRepository hashtagDocumentRepository;
+	private final ElasticsearchOperations elasticsearchOperations;
+
+	@Transactional
+	public List<HashtagResponseDto> migrateDataToElasticsearch() {
+		List<Hashtag> hashtags = hashtagRepository.findAll();
+		List<HashtagDocument> documents = hashtags.stream()
+			.map(HashtagDocument::createHashTagDocument)
+			.toList();
+
+		elasticsearchOperations.save(documents);
+
+		return hashtags.stream().map(HashtagResponseDto::of).toList();
+	}
+
+	public List<HashtagDocument> searchHashtags(String query) {
+		List<HashtagDocument> list = hashtagDocumentRepository.findByContentContaining(query);
+		list.forEach(System.out::println);
+		return list;
+	}
+
+}
