6a3739f2df3e2e6e484bcd141c18e95d8cbbd8af
DESKTOP-MMCQPV1\SSAFY <breakking@naver.com>
2024-10-08 17:38:45 +0900

    feat: 커뮤니티 화면, api 연동

--- frontend/melting/src/components/Music/SearchBar.tsx
+++ frontend/melting/src/components/Music/SearchBar.tsx
@@ -16,7 +16,7 @@ export default function SearchBar({
   placeholderText = 'Search...',
 }: SearchBarProps) {
   return (
-    <div className="flex w-full items-center justify-between rounded-full bg-gray-100 text-black px-4 py-1">
+    <div className="flex w-full items-center justify-between rounded-full bg-gray-100 text-black px-4 py-1 mt-3">
       <Input
         placeholder={placeholderText}
         value={searchTerm}
