91c5b5de008f49e90f1737efb8c64245cbb880e2
유승현 <fview@naver.com>
2024-10-08 11:06:34 +0900

    fix: redis 명반 저장 로직 수정
    Jira: S11P21A701-775

--- backend/src/main/java/com/dayangsung/melting/global/batch/BatchScheduler.java
+++ backend/src/main/java/com/dayangsung/melting/global/batch/BatchScheduler.java
@@ -1,4 +1,4 @@
-package com.dayangsung.melting.global.batch.scheduler;
+package com.dayangsung.melting.global.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
