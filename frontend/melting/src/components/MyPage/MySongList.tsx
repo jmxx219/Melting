ec8869d3e36a16066790743330d7b4b59236d6b9
손지민 <jimmi219@naver.com>
2024-10-08 11:27:02 +0900

    feat: 곡 리스트 조회 시, 무한 스크롤 적용

--- frontend/melting/src/components/MyPage/MySongList.tsx
+++ frontend/melting/src/components/MyPage/MySongList.tsx
@@ -1,50 +1,66 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import MySongContent from '@/components/MyPage/MySongContent'
 import { SortType } from '@/types/constType'
 import { SongListDto } from '@/types/user'
 import { userApi } from '@/apis/userApi'
+import InfiniteScroll from '@/components/Common/InfinityScroll.tsx'
 
 interface AlbumSongToggleProps {
   sortOption: SortType
 }
 
+const fetchMySongs = async (page: number = 0, pageSize: number = 10) => {
+  try {
+    const response = await userApi.getMemberSongs(page, pageSize)
+    return {
+      newItems: response.mySongList ?? [],
+      isLast: response.isLast ?? true,
+    }
+  } catch (error) {
+    console.error('Error fetching songs:', error)
+    return { newItems: [], isLast: true }
+  }
+}
+
 export default function MySongList({ sortOption }: AlbumSongToggleProps) {
   const [originalSongs, setOriginalSongs] = useState<SongListDto[]>([])
-  const [loading, setLoading] = useState<boolean>(false)
-  // const [isLast, setIsLast] = useState(false)
+  const [page, setPage] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [hasMore, setHasMore] = useState(true)
 
-  const fetchMySongs = async () => {
+  const loadMoreItems = useCallback(async () => {
+    if (loading || !hasMore) return
     setLoading(true)
-    try {
-      const response = await userApi.getMemberSongs()
-      if (response.mySongList) {
-        setOriginalSongs(response.mySongList)
-      }
-      console.log(response)
-      // setIsLast(response.isLast || false)
-    } catch (error) {
-      console.error('Error fetching songs:', error)
-    } finally {
-      setLoading(false)
-    }
-  }
+
+    const { newItems, isLast } = await fetchMySongs(page)
+
+    setOriginalSongs((prev) => [...prev, ...newItems])
+    setHasMore(!isLast)
+    setPage((prev) => prev + 1)
+    setLoading(false)
+  }, [loading, hasMore, page])
 
   useEffect(() => {
-    fetchMySongs()
+    setOriginalSongs([])
+    setPage(0)
+    setHasMore(true)
+    loadMoreItems()
   }, [sortOption])
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
 
   return (
     <div>
-      {originalSongs.map((originalSong) => (
-        <MySongContent
-          key={originalSong.originalSongId}
-          originalSong={originalSong}
-        />
-      ))}
+      <InfiniteScroll
+        loadMore={loadMoreItems}
+        hasMore={hasMore}
+        loading={loading}
+      >
+        {originalSongs.map((originalSong) => (
+          <MySongContent
+            key={originalSong.originalSongId}
+            originalSong={originalSong}
+          />
+        ))}
+      </InfiniteScroll>
     </div>
   )
 }
