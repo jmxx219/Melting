a50a6e2a99b6c92dc40c766c7b67bcb155a33625
이보연 <btothey99@ewhain.net>
2024-10-09 19:52:23 +0900

    feat: PWA 설정
    Jira: S11P21A701-828

--- frontend/melting/vite.config.ts
+++ frontend/melting/vite.config.ts
@@ -1,10 +1,35 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Melting',
+        short_name: '멜팅',
+        description: '나만의 AI 커버 앨범을 만들어보세요.',
+        theme_color: '#ffaf25',
+        icons: [
+          {
+            src: 'icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: '/',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
