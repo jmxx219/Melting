a32008f845905aff5ba208f42359595db01df80d
이보연 <btothey99@ewhain.net>
2024-10-10 09:43:34 +0900

    style: css 수정
    Jira: S11P21A701-828

--- frontend/melting/src/components/ui/textarea.tsx
+++ frontend/melting/src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex min-h-[80px] w-full rounded-sm border border-gray-300 bg-background px-3 py-2 text-sm placeholder:text-gray-300 focus-visible:outline-none focus-visible:border-primary-400 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex min-h-[80px] w-full rounded-sm border border-gray-300 bg-background px-3 py-2 text-sm placeholder:text-gray-300 focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-400 disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
